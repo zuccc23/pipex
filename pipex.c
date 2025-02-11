ed6ae863ddf04f38437a663d762f67e9309bde0a
zuccc23 <daniyaahmane@gmail.com>
2025-02-11 18:10:40 +0100

    testing fork and pipe

--- pipex.c
+++ pipex.c
@@ -1,8 +1,41 @@
-#include "pipex.h"
-#include <stdio.h>
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dahmane <dahmane@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 00:54:28 by dahmane           #+#    #+#             */
+/*   Updated: 2025/02/11 18:07:51 by dahmane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main (void)
+#include "pipex.h"
+
+int main (int argc, char **argv)
 {
-    printf("hello");
+    int id = 0;
+    int fd[2];
+    int nb = 22;
+    int nb2;
+    
+    // fd[0] = open(argv[1], O_RDONLY);
+    // fd[1] = open(argv[2], O_RDONLY);
+    pipe(fd);
+    id = fork();
+    if (id == 0)
+    {
+        close(fd[0]);
+        write(fd[1], &nb, sizeof(int));
+        close(fd[1]);
+    }
+    if (id != 0)
+    {
+        close(fd[1]);
+        read(fd[0], &nb2, sizeof(int));
+        printf("%d\n", nb2);
+        close(fd[0]);
+    }
+    
+    
 }
--- pipex.h
+++ pipex.h
@@ -1,2 +1,24 @@
-#include <unistd.h>
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dahmane <dahmane@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 00:54:46 by dahmane           #+#    #+#             */
+/*   Updated: 2025/02/11 17:40:50 by dahmane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <fcntl.h> 
+# include <string.h>
+
+#endif
